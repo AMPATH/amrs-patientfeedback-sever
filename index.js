18b523924fd839da18501f14b6d7799a0e732e32
jecihjoy <jecihjoy@gmaail.com.com>
2018-10-02 14:59:42 +0300

    removed server side authenitacation

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const Hapi = require('hapi');
 const routes = require('./routes');
 const config = require('./config/config');
-const connection =  require('./connection/connection');
+const connection = require('./connection/connection');
 
 const server = Hapi.Server({
   port: config.application.port,
@@ -13,42 +13,12 @@ const server = Hapi.Server({
   }
 });
 
-const validate = async (_request, username, password) => {
-  // var authBuffer = new Buffer(username + ":" + password).toString("base64");
-  // var headers = {'Authorization': "Basic " + authBuffer};
-  return new Promise(
-    (resolve, reject) => {
-      if (username === 'jgichuhi' && password === 'Jgichuhi@2018') resolve({isValid: true, credentials: {}})
-      else resolve({isValid: false, credentials: {}})});
-    //   var callback = (error, response, _body) => {
-    //     if (error) reject(error);
-    //     const data = JSON.parse(response.body);
-    //     resolve({isValid: data.authenticated, credentials: {}});
-    //   }
-
-    //   request(
-    //     { method: 'GET',
-    //       url: 'https://ngx.ampath.or.ke/test-amrs/ws/rest/v1/session/',
-    //       headers: headers
-    //     }, callback
-    //   );
-    // });
-  };
 
 const init = async () => {
-  
-  await server.register([
-    {plugin: require('hapi-auth-basic')},
-    {plugin: require('inert')}
-  ]);
 
-  server.auth.strategy('simple', 'basic', { validate });
-
-  for (var route of routes.routesFxn(validate)) {
+  for (var route of routes.routesFxn()) {
     server.route(route);
   }
-
-  server.auth.default('simple');
 
   await server.start();
   console.log('Server is running');
